5dfc14629e28d31586ea52f6a96904fc21eb7001
Egor Martynenko <egor.martinenko01@gmail.com>
2024-04-29 12:55:48 +0300

    first commit

---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGithubUser } from 'types';
+
+export const defaultUser: LocalGithubUser = {
+  "login": "egor-martynenko",
+  "avatar": "https://avatars.githubus…tent.com/u/109789230?v=4",
+  "name": "Egor Martynenko",
+  "company": "",
+  "blog": "martynenko.com",
+  "location": "",
+  "bio": "React developer",
+  "twitter": "",
+  "repos": 7,
+  "followers": 0,
+  "following": 0,
+  "created": "2022-07-22T07:43:39Z",
+}
